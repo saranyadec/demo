1d85572a26563d609d7c909a4946eb3903e525e3
saranyadec <110887571+saranyadec@users.noreply.github.com>
2026-03-19 17:18:32 +0530

    sh file added

---
+++ hello.sh
@@ -0,0 +1,3 @@
+#!/bin/bash echo “Hello World from Jenkins!”[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C
+[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
